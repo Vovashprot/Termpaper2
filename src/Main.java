ceaa3f730359d2fafd7d9900f38b9206710e808f
Vovashprot <vova.protasenko@gmail.com>
2023-03-13 17:23:03 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static  int square(int num){
+        num = num*num;
+        return num;
+    }
+    public static void main(String[] args) {
+        System.out.println(square(3));
+
+
+    }
 }
